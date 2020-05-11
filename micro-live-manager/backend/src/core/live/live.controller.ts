9a6bb3cd59601e6fb99f954297836ab810d9a2c1
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-11 13:48:31 -0300

    chore: add methods on LiveController

--- micro-live-manager/backend/src/core/live/live.controller.ts
+++ micro-live-manager/backend/src/core/live/live.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { LiveService } from './live.service';
+import { Controller, Get, Param, Post, Req } from '@nestjs/common';
 
-@Controller('live')
+@Controller('lives')
 export class LiveController {
 
+  constructor(private liveService: LiveService) {}
 
+  @Get()
+  async index() {
+    return await this.liveService.findAll();
+  }
+  
+  @Get(':slug')
+  async show(@Param('slug') slug: string) {
+    return await this.liveService.showlive(slug);
+  }
+
+  @Post()
+  async store(@Req() req: Request) {
+    return await this.liveService.create(req.body);
+  }
 }
