66c208edbd688f68141532a01605442393f796a4
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-17 17:53:31 -0300

    chore: add form for transmitions live

--- micro-live-streaming/frontend/src/pages/Broadcast/broadcast.js
+++ micro-live-streaming/frontend/src/pages/Broadcast/broadcast.js
@@ -2,15 +2,76 @@ import React, { Component } from 'react';
 import './broadcast.css';
 
 import ContainerVideo from '../../components/container-video/ContainerVideo';
+import CustomInput from '../../components/CustomInput/CustomInput';
+import CreateButton from '../../components/CustomButton/CustomButton';
 import NavBroadcast from '../../components/nav/NavBroadcast';
+import Modal from 'react-modal';
+
+Modal.setAppElement('body');
 
 class Broadcast extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      modal: true,
+      name: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  openModal = () => {
+    this.setState({modal: true});
+  }
+
+  closeModal = () => {
+    this.setState({modal: false});
+  }
 
   render() {
     return (
       <div>
         <NavBroadcast />
         <ContainerVideo />
+
+        <Modal
+          isOpen={this.state.modal}
+          onRequestClose={this.closeModal}
+          contentLabel="Example Modal">
+        
+          <form>
+            <div className="container-form">
+              <h1 style={{ fontWeight: 800, fontSize: 26, textAlign: 'center' }}>Broadcast live</h1>
+
+              <CustomInput
+                classs={'mt-40'}
+                type={'text'}
+                placeholder={'Name'}
+              />
+
+              <CustomInput
+                classs={'mt-40'}
+                type={'text'}
+                placeholder={'E-mail'}
+              />
+
+              <CustomInput
+                classs={'mt-40'}
+                type={'password'}
+                placeholder={'Password'}
+              />
+
+              <CreateButton
+                typeBtn="submit"
+                className={'btn btn-outlined purple-btn'}
+                children={'Done'}
+              />
+              
+            </div>
+          </form>
+        </Modal>
       </div>
     )
   }
