e1d366102517ffc2b48815e8dd2cf105dde44600
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-19 15:28:48 -0300

    feature: add hooks, useBroadcast

---
+++ micro-live-streaming/frontend/src/hooks/useBroadcast.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
+import io from "socket.io-client";
+import Peer from "peerjs";
+
+const useBroadcast = (data) => {
+
+  const { start, stop, liveSlug, password, videoRef } = data;
+
+  const [error, setError] = useState({message: '', name: ''});
+  const [usersConnected, setUserConnected] = useState(0);
+  const peerRef = useRef();
+  const streamRef = useRef();
+
+  const socket = useMemo(() => {
+    if (!start) { return null; }
+    return io(`${process.env.REACT_APP_MICRO_BACKEND_MANAGER_URL}/live`);
+  }, [start]);
+
+  useEffect(() => {
+
+    if (!socket) { return; }
+
+    socket.on('connect', () => {
+
+      //socket.emit('join', {slug: liveSlug});
+
+      socket.on('count-users', (count) => {
+        setUserConnected(count);
+      });
+    });
+
+  }, [liveSlug, socket]);
+
+  return { usersConnected }
+}
+
+export default useBroadcast;
--- micro-live-streaming/frontend/src/pages/Broadcast/broadcast.js
+++ micro-live-streaming/frontend/src/pages/Broadcast/broadcast.js
@@ -1,167 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './broadcast.css';
 
 import ContainerVideo from '../../components/container-video/ContainerVideo';
 import CustomButton from '../../components/CustomButton/CustomButton';
 import CustomInput from '../../components/CustomInput/CustomInput';
 import NavBroadcast from '../../components/nav/NavBroadcast';
-import getIceServers from '../../utils/get.ice.server';
-import { base, getLive } from '../../service/Api';
 import Modal from 'react-modal';
-
-import socket from "socket.io-client";
-import Peer from "peerjs";
 
 Modal.setAppElement('body');
 
-class Broadcast extends Component {
+const Broadcast = () => {
 
-  constructor() {
-    super();
+  const [openModal, setOpenModal] = useState(false);
 
-    this.state = {
-      modal: true,
-      name: '',
-      email: '',
-      password: '',
-      error: '',
-      socket: null,
-      peer: null,
-      countUsers: 0
-    }
-  }
+  return (
+    <div>
+      <NavBroadcast />
+      <ContainerVideo />
 
-  handleChange = (event) => {
-    event.preventDefault();
-    this.setState({ [event.target.name]: event.target.value});
-  }
-  
-  handlerClick = (event) => {
-    event.preventDefault();
+      <Modal
+        isOpen={ openModal }
+        onRequestClose={ setOpenModal(true) }
+        contentLabel="Example Modal">
+        
+        <form >
+          <div className="container-form">
+            <h1 style={{ fontWeight: 800, fontSize: 26, textAlign: 'center' }}>Broadcast live</h1>
 
-    const { name, email, password } = this.state;
-    
-    if(!password) {
-      this.setState(() => ({ error: 'Fill in all the fields' }));
-    } else {
-      this.setState(() => ({password: password}));
-    }
-  }
+            <CustomInput
+              classs={'mt-40'}
+              type={'text'}
+              placeholder={'Name'}
+              name={'name'}
+            />
 
-  onSubmitClean() {
-    this.setState({name: '', email: '', password: ''});
-  }
+            <CustomInput
+              classs={'mt-40'}
+              type={'text'}
+              placeholder={'E-mail'}
+              name={'email'}
+            />
 
-  onSubmitFailure() {
-    this.setState({ error: "Request Failed" });
-  }
+            <CustomInput
+              classs={'mt-40'}
+              type={'password'}
+              placeholder={'Password'}
+              name={'password'}
+            />
 
-  openModal = () => {
-    this.setState({modal: true});
-  }
-
-  closeModal = () => {
-    this.setState({modal: false});
-  }
-
-  componentWillMount() { 
-    this.initSocketAndPeer();
-  }
-
-  initSocketAndPeer = () => { 
-    console.log('Initialize [socket] [peer] connection...');
-
-    const currentSocket = socket(`${process.env.REACT_APP_MICRO_BACKEND_MANAGER_URL}/live`);
-  
-    const currentPeer = new Peer({
-      host: process.env.REACT_APP_MICRO_GENERATOR_PEER_DOMAIN,
-      port: parseInt(process.env.REACT_APP_MICRO_GENERATOR_PEER_PORT)
-    });
-
-    this.setState(() => ({socket: currentSocket, peer: currentPeer}));
-  }
-
-  broadcastSocket = (socket) => {
-    const { slug } = this.props.match.params;
-   
-    socket.on('connect', () => {
-
-      socket.emit('join', {slug: `${slug}`});
-
-      socket.on('count-users', (count) => {
-        this.setState(() => ({countUsers: count}));
-      });
-    });
-  }
-
-  broadcastPeer = (peer, socket) => {
-
-    peer.on('open', (peer_id) => {
-      console.log('BoradcastPeer id: ', peer_id);
-      socket.emit('set-broadcaster', {client_id: peer_id, password: '123'});
-    });
-  }
-
-  render() {
-
-    const { name, email, password, socket, peer } = this.state;
- 
-    this.broadcastSocket(socket);
-    this.broadcastPeer(peer, socket);
- 
-    return (
-      <div>
-        <NavBroadcast />
-        <ContainerVideo />
-
-        <Modal
-          isOpen={this.state.modal}
-          onRequestClose={this.closeModal}
-          contentLabel="Example Modal">
-          
-          <form onSubmit={ this.handlerClick }>
-            <div className="container-form">
-              <h1 style={{ fontWeight: 800, fontSize: 26, textAlign: 'center' }}>Broadcast live</h1>
-
-              <CustomInput
-                classs={'mt-40'}
-                type={'text'}
-                placeholder={'Name'}
-                value={name}
-                name={'name'}
-                onChange={this.handleChange}
-              />
-
-              <CustomInput
-                classs={'mt-40'}
-                type={'text'}
-                placeholder={'E-mail'}
-                value={email}
-                name={'email'}
-                onChange={this.handleChange}
-              />
-
-              <CustomInput
-                classs={'mt-40'}
-                type={'password'}
-                placeholder={'Password'}
-                value={password}
-                name={'password'}
-                onChange={this.handleChange}
-              />
-
-              <CustomButton
-                typeBtn="submit"
-                className={'btn btn-outlined purple-btn'}
-                children={'Done'}
-              />
-              
-            </div>
-          </form>
-        </Modal>
-      </div>
-    )
-  }
+            <CustomButton
+              typeBtn="submit"
+              className={'btn btn-outlined purple-btn'}
+              children={'Done'}
+            />
+            
+          </div>
+        </form>
+      </Modal>
+    </div>
+  )
 }
 
 export default Broadcast;
