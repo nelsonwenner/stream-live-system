8439d052672b71603a29c49f0004eae89b8b074a
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-27 15:06:03 -0300

    chore: add entity ChatMessageEntity

---
+++ micro-live-chat/src/database/entities/chat.menssage.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+@Entity({name: 'chat_messages'})
+export class ChatMessageEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  
+  @Column()
+  content: string;
+
+  @Column()
+  user_name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  live_slug: string;
+
+  @Column()
+  is_broadcaster: boolean;
+
+  @CreateDateColumn({type: "timestamp"})
+  created_at: Date;
+}
--- micro-live-chat/src/main.ts
+++ micro-live-chat/src/main.ts
@@ -7,6 +7,7 @@ import 'dotenv/config';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, {cors: true});
   app.useWebSocketAdapter(new RedisIoAdapter(app));
+  app.setGlobalPrefix('api');
   
   await app.listen(process.env.SERVER_PORT, () => {
     Logger.log(`Server running on http://localhost:${process.env.SERVER_PORT}`, 'Bootstrap');
