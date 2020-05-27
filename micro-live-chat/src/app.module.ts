db15d6176ea1baa061d3a4177c481a3bf6f70eca
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-27 19:05:01 -0300

    chore: configured connection with rabbitmq

--- micro-live-chat/src/app.module.ts
+++ micro-live-chat/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import * as ormOptions from './config/ormconfig';
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ChatModule } from './core/chat/chat.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormOptions),
+    ChatModule
   ],
 })
 
