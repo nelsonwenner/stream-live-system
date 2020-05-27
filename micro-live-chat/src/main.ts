32aa3a3c58e5e554c954fa64eed1f52b9ebec0bf
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-27 14:52:48 -0300

    chore: configured redis

--- micro-live-chat/src/main.ts
+++ micro-live-chat/src/main.ts
@@ -1,3 +1,4 @@
+import { RedisIoAdapter } from './core/redis-io-adapter/redis-io.adapter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
@@ -5,6 +6,7 @@ import 'dotenv/config';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, {cors: true});
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
   
   await app.listen(process.env.SERVER_PORT, () => {
     Logger.log(`Server running on http://localhost:${process.env.SERVER_PORT}`, 'Bootstrap');
