d2d93a9a69251076cd5ee57eba00a79714e38070
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-26 22:34:38 -0300

    refactor: refactor margin-top from table of lives

--- micro-live-manager/frontend/src/routes/routes.js
+++ micro-live-manager/frontend/src/routes/routes.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Router} from 'react-router-dom';
+import {history} from './history';
 
 import Dashboard from '../pages/Dashboard/dashboard';
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <Router history={ history }>
       <Switch>
         <Route exact path="/" component={ Dashboard } />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
