9c73b7b044f86b1d1d0189d083a2c8b90ef2f1d3
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-28 12:25:40 -0300

    refat(NewLiveModal): refactored NewLiveModal Component

--- micro-live-manager/frontend/src/components/common/NewLiveModal/index.jsx
+++ micro-live-manager/frontend/src/components/common/NewLiveModal/index.jsx
@@ -1,39 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles.css';
 
 import redirect from '../../../routes/redirect';
-import CreateButton from '../CreateButton';
+import { useForm } from "react-hook-form";
 import CustomInput from '../CustomInput';
 import Api from '../../../service/Api';
 import Modal from 'react-modal';
+import * as yup from 'yup';
 
 Modal.setAppElement('body');
 
+const validationSchema = yup.object().shape({
+  title: yup.string().label('Title').required(),
+  description: yup.string().label('Description').required(),
+  date: yup.string().label('Date').required(),
+  password: yup.string().label('Password').required(),
+});
+
 const NewLiveModal = ({ openModal, closeModal }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const { register, handleSubmit, errors } = useForm({
+    validationSchema: validationSchema,
+  });
 
-    if(!title || !password || !description || !date) {
-      setError('Fill in all the fields');
-    } else {
-      Api.post('/api/lives', {title, description, date, password})
-      .then((res) => {
-      
-        redirect('/');
-
-      })
-      .catch(() => onSubmitFailure());
-    }
-  }
-
-  const onSubmitFailure = () => {
-    setError("Request Failed" );
+  const onSubmit = (data) => {
+    Api.post('/api/lives', data)
+    .then((res) => {
+      redirect('/');
+    });
   }
 
   return (
@@ -42,60 +35,46 @@ const NewLiveModal = ({ openModal, closeModal }) => {
       onRequestClose={ closeModal }
       contentLabel="Example Modal">
       
-      <form method="post" onSubmit={ onSubmit }>
-        <div className="container-form">
-          <h1 className="title-modal">New live</h1>
-
+      <form onSubmit={ handleSubmit(onSubmit) } className="form-new-live">
+        <h1 className="title-modal">New live</h1>
+        <div className="container-input">
           <CustomInput
-            classs={'mt-17'}
-            type={'text'}
-            placeholder={'Title'}
-            value={title}
-            name={'title'}
-            onChange={ (event) => setTitle(event.target.value) }
+            classs={ 'width-80 mt-25' }
+            type={ 'text' }
+            name={ 'title' }
+            placeholder={ 'Title' }
+            errors={ errors.title }
+            register={ register }
           />
 
           <CustomInput
-            classs={'mt-17'}
+            classs={ 'width-80 mt-25' }
             type={'text'}
-            placeholder={'Description'}
-            value={description}
             name={'description'}
-            onChange={ (event) => setDescription(event.target.value) }
+            placeholder={'Description'}
+            errors={ errors.description }
+            register={ register }
           />
 
           <CustomInput
-            classs={'mt-17'}
+            classs={ 'width-80 mt-25' }
             type={'date'}
-            placeholder={''}
-            value={date}
             name={'date'}
-            onChange={ (event) => setDate(event.target.value) }
+            placeholder={''}
+            errors={ errors.date }
+            register={ register }
           />
 
           <CustomInput
+            classs={ 'width-80' }
             type={'password'}
-            placeholder={'Password'}
-            value={password}
             name={'password'}
-            onChange={ (event) => setPassword(event.target.value) }
+            placeholder={'Password'}
+            errors={ errors.password }
+            register={ register }
           />
-
-          <CreateButton
-            typeBtn="submit"
-            className={'btn btn-outlined purple-btn mt-30'}
-            children={'Create'}
-          />
-
-          {
-            error && (
-              
-              <div className="error">
-                <p style={{color: 'red'}}>{ error }</p>
-              </div>
-
-            )
-          }
+          
+          <button className="btn btn-rounded black-btn btn-outlined">Create</button>
         </div>
       </form>
     </Modal>
