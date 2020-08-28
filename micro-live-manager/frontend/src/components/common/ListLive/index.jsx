949283550097de4b69f246329c3cc3cadbd8aee7
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-28 13:56:00 -0300

    feat(effect-in): added effect-in in list lives

--- micro-live-manager/frontend/src/components/common/ListLive/index.jsx
+++ micro-live-manager/frontend/src/components/common/ListLive/index.jsx
@@ -12,8 +12,8 @@ const Listlive = ({ lives }) => {
             <div className="uuid-live" data-label="UUID">{live.id}</div>
             <div className="title-live" data-label="TITLE">{live.title}</div>
             <div className="date-live" data-label="DATE" >{ format(parseISO(live.created_at), 'dd/MM/yyyy HH:mm')}</div>
-            <div className="create-link-live" data-label="CREATE LIVE"> <a target="_blank" rel="noopener noreferrer" href={`http://localhost:4000/broadcast/${live.slug}`}>create live</a> </div>
-            <div className="invite-live" data-label="INVITE"> <a target="_blank" rel="noopener noreferrer" href={`http://localhost:4000/viewer/${live.slug}`}>invite</a> </div>
+            <div className="create-link-live effect-in" data-label="CREATE LIVE"> <a target="_blank" rel="noopener noreferrer" href={`http://localhost:4000/broadcast/${live.slug}`}>create live</a> </div>
+            <div className="invite-live effect-in" data-label="INVITE"> <a target="_blank" rel="noopener noreferrer" href={`http://localhost:4000/viewer/${live.slug}`}>invite</a> </div>
             <div className="status-live" data-label="STATUS">
               <div className="circle-status-live" style={{ backgroundColor: live.status === 'pending' ? 'rgb(255, 208, 86)' : 'red' }}></div>
             </div>
--- micro-live-manager/frontend/src/components/common/TableHeader/index.jsx
+++ micro-live-manager/frontend/src/components/common/TableHeader/index.jsx
@@ -7,7 +7,7 @@ const TableHeader = () => {
       <div className="text uuid">UUID</div>
       <div className="text title-bar">TITLE</div>
       <div className="text date">DATE</div>
-      <div className="text create-live">CREATE LIVE</div>
+      <div className="text create-live custom-field">CREATE LIVE</div>
       <div className="text invite">INVITE</div>
       <div className="text status">STATUS</div>
     </div>
